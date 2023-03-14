b80ee5ca8d22e8f0854bccc5835820c2e6bd3d28
awusan125 <74240688+awusan125@users.noreply.github.com>
2023-03-14 19:14:31 +0800

    add output and input

---
+++ output.py
@@ -0,0 +1,11 @@
+import sys
+
+class output():
+    def __init__(self):
+        self.forward=[]
+        self.rotate=[]
+        self.buy=[]
+        self.sell=[]
+        self.destroy=[]
+    def getForward:
+        self.forward.append
